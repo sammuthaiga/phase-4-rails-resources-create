362fe8f9541c5c51865aeaad08f69d8b7015b58a
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-03-16 23:55:17 +0300

    Completed assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,4 +1,9 @@
 class BirdsController < ApplicationController
+  #POST /birds
+  def create
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
+  end
 
   # GET /birds
   def index
@@ -15,5 +20,4 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
-
 end
